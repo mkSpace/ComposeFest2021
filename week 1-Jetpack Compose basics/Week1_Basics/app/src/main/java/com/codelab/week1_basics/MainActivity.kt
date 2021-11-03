865523f9cf77a4a6e0a2fb01790512b5e82c217d
Jaemin <once29@naver.com>
2021-11-03 20:36:11 +0900

    Replace Button to IconButton

--- week 1-Jetpack Compose basics/Week1_Basics/app/src/main/java/com/codelab/week1_basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/Week1_Basics/app/src/main/java/com/codelab/week1_basics/MainActivity.kt
@@ -11,10 +11,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -98,8 +102,11 @@ fun Greeting(name: String) {
                     )
                 )
             }
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
-                Text("Show ${if (expanded.value) "less" else "more"}")
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(
+                    imageVector = if (expanded.value) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = stringResource(if (expanded.value) R.string.show_less else R.string.show_more)
+                )
             }
         }
     }
